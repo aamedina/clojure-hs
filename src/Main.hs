216725bd5a4ff420395a18d059ba4509be901111
Adrian Medina <adrian.medina@mail.yu.edu>
2014-08-07 18:07:57 -0400

    basic repl that prints input

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,16 @@
 module Main where
+import System.Console.Haskeline
 
 main :: IO ()
-main = putStrLn "Hello, world!"
+main = runInputT defaultSettings loop where
+  loop :: InputT IO ()
+  loop = do
+    line <- getInputLine "=> "
+    case line of
+     Nothing -> return ()
+     Just "(exit)" -> return ()
+     Just "exit" -> return ()
+     Just line -> do
+       outputStrLn line
+       loop
+  
